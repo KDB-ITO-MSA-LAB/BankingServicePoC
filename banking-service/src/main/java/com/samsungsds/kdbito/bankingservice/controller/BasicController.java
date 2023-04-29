396c7b6a3ef7570d9f351e0710b7685a0aa3d54f
hunyfuny <hguony76@gmail.com>
2023-04-30 08:15:54 +0900

    Init Project

---
+++ banking-service/src/main/java/com/samsungsds/kdbito/bankingservice/controller/BasicController.java
@@ -0,0 +1,15 @@
+package com.samsungsds.kdbito.bankingservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+	
+	@GetMapping("/msalab")
+	public String defaultFunction() {
+		
+		return "welcome";
+	}
+
+}
